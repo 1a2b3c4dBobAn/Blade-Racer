5e0baa4939e4863effe63db8bb87ec9083b93fda
BobAn <1a2b3c4d@berkeley.edu>
2018-06-18 10:40:27 -0700

    working on commonjs

--- common.js
+++ common.js
@@ -10,6 +10,30 @@ var Dom = {
 
 var Util = {
 
+  timestamp:        function()                  { return new Date().getTime();                                    },
+  toInt:            function(obj, def)          { if (obj !== null) { var x = parseInt(obj, 10); if (!isNaN(x)) return x; } return Util.toInt(def, 0); },
+  toFloat:          function(obj, def)          { if (obj !== null) { var x = parseFloat(obj);   if (!isNaN(x)) return x; } return Util.toFloat(def, 0.0); },
+  limit:            function(value, min, max)   { return Math.max(min, Math.min(value, max));                     },
+  randomInt:        function(min, max)          { return Math.round(Util.interpolate(min, max, Math.random()));   },
+  randomChoice:     function(options)           { return options[Util.randomInt(0, options.length-1)];            },
+  percentRemaining: function(n, total)          { return (n%total)/total;                                         },
+  accelerate:       function(v, accel, dt)      { return v + (accel * dt);                                        },
+  interpolate:      function(a,b,percent)       { return a + (b-a)*percent                                        },
+  easeIn:           function(a,b,percent)       { return a + (b-a)*Math.pow(percent,2);                           },
+  easeOut:          function(a,b,percent)       { return a + (b-a)*(1-Math.pow(1-percent,2));                     },
+  easeInOut:        function(a,b,percent)       { return a + (b-a)*((-Math.cos(percent*Math.PI)/2) + 0.5);        };
+
+  increase:  function(start, increment, max) { // with looping
+    var result = start + increment;
+    while (result >= max)
+      result -= max;
+    while (result < 0)
+      result += max;
+    return result;
+  },
+
+
+
 }
 
 
@@ -30,6 +54,59 @@ if (!window.requestAnimationFrame) {
 //Game loop helper
 
 var Game = {
+  run: function(options){
+    Game.loadImages(options.images, function(images){
+       options.ready(images);
+       Game.setKeyListener(options.keys);
+
+      var canvas = options.canvas,
+          update = options.update,
+          render = options.render,
+          step   = options.step,
+          stats  = options.stats,
+          now    = null,
+          last   = Util.timestamp(),
+          dt     = 0,
+          gdt    = 0;
+
+      function frame(){
+        now = Util.timestamp();
+        dt = Math.min(1, (now - last)/ 1000);
+        gdt = gdt + dt;
+        while (gdt > step){
+          gdt = gdt - step;
+          update(step);
+        }
+        render();
+        // stats.update();
+        last = now;
+        requestAnimationFrame(frame);
+      }
+      frame();
+    })
+  },
+
+
+
+
+  loadImages: function(names, callback) {
+    var result = [];
+    var count  = names.length;
+
+    var onload = function() {
+      if (--count == 0)
+        callback(result);
+    };
+
+    for(var n = 0 ; n < names.length ; n++) {
+      var name = names[n];
+      result[n] = document.createElement('img');
+      Dom.on(result[n], 'load', onload);
+      result[n].src = "images/" + name + ".png";
+    }
+  },
+
+
 
 }
 
